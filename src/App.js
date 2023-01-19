ad81159efdf9ea037a2075ae1763b374702e5594
f190232 <f190232@cfd.nu.edu.pk>
2023-01-19 23:52:43 +0500

    Write in Database

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import { db } from './firebase'
 import { uid } from 'uid';
+import { set, ref } from 'firebase/database';
+import { useState, useEffect } from 'react';
 
 function App() {
+const [todo, setTodo] = useState("");
+const handleTodoChange= () => {
+  setTodo(e.target.value)
+}
+  //write
+  const writeToDatabase = () => {
+    const uuid = uid()
+    set(ref(db, '/${uuid'), {
+      todo, 
+      uuid,
+    });
+
+    setTodo("");
+  };
   return (
     <div className="App">
-       
+      <input type="text" value={todo} onChange={handleTodoChange}/>
+      <button onClick={writeToDatabase}>submit</button>       
     </div>
-  );
+  ); 
 }
 
 export default App;
