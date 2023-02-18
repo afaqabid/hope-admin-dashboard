9cdd437fd417a5de6390e318c41949ab188108b4
Afaq Abid <afaqabid@gmail.com>
2023-02-18 21:29:57 +0500

    Added Config in Dashboard Nav.

---
+++ src/layouts/dashboard/nav/config.js
@@ -0,0 +1,48 @@
+// component
+import SvgColor from '../../../components/svg-color';
+import Iconify from '../../../components/iconify';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+const navConfig = [
+  // {
+  //   title: 'dashboard',
+  //   path: '/dashboard/app',
+  //   icon: icon('ic_analytics'),
+  // },
+  {
+    title: 'user',
+    path: '/dashboard/user',
+    icon: icon('ic_user'),
+  },
+  // {
+  //   title: 'product',
+  //   path: '/dashboard/products',
+  //   icon: icon('ic_cart'),
+  // },
+  {
+    title: 'blog',
+    path: '/dashboard/blog',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'help alerts',
+    path: '/dashboard/helpAlerts',
+    // icon: icon('ic_notification_mail'),
+    icon: <Iconify icon="eva:bell-fill" />,
+  },
+  // {
+  //   title: 'login',
+  //   path: '/login',
+  //   icon: icon('ic_lock'),
+  // },
+  // {
+  //   title: 'Not found',
+  //   path: '/404',
+  //   icon: icon('ic_disabled'),
+  // },
+];
+
+export default navConfig;
