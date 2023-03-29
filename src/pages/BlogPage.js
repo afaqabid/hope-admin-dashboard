36effc4ff397f81207d263464876852db6f1f356
Afaq Abid <afaqabid@gmail.com>
2023-03-29 22:22:18 +0500

    Bugs Fixed.

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,31 +1,33 @@
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from "react-helmet-async";
 // @mui
-import { Grid, Button, Container, Stack, Typography } from '@mui/material';
+import { Grid, Button, Container, Stack, Typography } from "@mui/material";
 // components
-import Iconify from '../components/iconify';
-import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
+import Iconify from "../components/iconify";
+import {
+  BlogPostCard,
+  BlogPostsSort,
+  BlogPostsSearch,
+} from "../sections/@dashboard/blog";
 // mock
-import POSTS from '../_mock/blog';
-import { useNavigate } from 'react-router-dom';
+import POSTS from "../_mock/blog";
+import { useNavigate } from "react-router-dom";
 
 // ----------------------------------------------------------------------
 
 const SORT_OPTIONS = [
-  { value: 'latest', label: 'Latest' },
-  { value: 'popular', label: 'Popular' },
-  { value: 'oldest', label: 'Oldest' },
+  { value: "latest", label: "Latest" },
+  { value: "popular", label: "Popular" },
+  { value: "oldest", label: "Oldest" },
 ];
 
 // ----------------------------------------------------------------------
 
 export default function BlogPage() {
-
   const navigate = useNavigate();
 
   const newPostbtn = () => {
-    navigate('/dashboard/newpost', { replace: true });
-
-  }
+    navigate("/dashboard/newpost", { replace: true });
+  };
 
   return (
     <>
@@ -34,19 +36,28 @@ export default function BlogPage() {
       </Helmet>
 
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Typography variant="h3" gutterBottom>
             Blog
           </Typography>
-          <Button variant="contained" onClick={newPostbtn} startIcon={<Iconify icon="eva:plus-fill" />}>
+          <Button
+            variant="contained"
+            onClick={newPostbtn}
+            startIcon={<Iconify icon="eva:plus-fill" />}
+          >
             New Post
           </Button>
         </Stack>
 
-        <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
+        {/* <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
           <BlogPostsSearch posts={POSTS} />
           <BlogPostsSort options={SORT_OPTIONS} />
-        </Stack>
+        </Stack> */}
 
         <Grid container spacing={3}>
           {POSTS.map((post, index) => (
