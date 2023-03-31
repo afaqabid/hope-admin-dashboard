3eefd60bff1840b3e7eb4b40678c145c75b05a0d
Afaq Abid <afaqabid@gmail.com>
2023-04-01 01:35:32 +0500

    Fixed Bugs.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import Router from './routes';
-import ThemeProvider from './theme';
-import ScrollToTop from './components/scroll-to-top';
-import { StyledChart } from './components/chart';
-import { AuthProvider } from './auth';
+import Router from "./routes";
+import ThemeProvider from "./theme";
+import ScrollToTop from "./components/scroll-to-top";
+import { AuthProvider } from "./auth";
 
 // ----------------------------------------------------------------------
 
 export default function App() {
   return (
     <ThemeProvider>
-        <StyledChart />
-        <Router />
-      </ThemeProvider>
+      <Router />
+    </ThemeProvider>
   );
 }
