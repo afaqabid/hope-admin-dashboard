3f9c59ad216832427d268db870e2499b432211a5
Afaq Abid <afaqabid@gmail.com>
2023-04-01 01:39:32 +0500

    Updated Help Alerts.

--- src/pages/HelpAlerts.js
+++ src/pages/HelpAlerts.js
@@ -1,33 +1,53 @@
-import { Helmet } from 'react-helmet-async';
-import { Grid, Button, Container, Stack, Typography, TextField, Divider, FormControl } from '@mui/material';
-import Iconify from '../components/iconify';
-import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
-import POSTS from '../_mock/blog';
-import { useNavigate } from 'react-router-dom';
-import { PreviousAlertsCard } from '../sections/@dashboard/helpAlerts';
-import emailjs from '@emailjs/browser';
+import { Helmet } from "react-helmet-async";
+import {
+  Grid,
+  Button,
+  Container,
+  Stack,
+  Typography,
+  TextField,
+  Divider,
+  FormControl,
+} from "@mui/material";
+import Iconify from "../components/iconify";
+import {
+  BlogPostCard,
+  BlogPostsSort,
+  BlogPostsSearch,
+} from "../sections/@dashboard/blog";
+import POSTS from "../_mock/blog";
+import { useNavigate } from "react-router-dom";
+import { PreviousAlertsCard } from "../sections/@dashboard/helpAlerts";
+import emailjs from "@emailjs/browser";
 
 // ----------------------------------------------------------------------
 
 export default function HelpAlerts() {
-
   const navigate = useNavigate();
 
   const newPostbtn = () => {
-    navigate('/dashboard/newpost', { replace: true });
-
-  }
+    navigate("/dashboard/newpost", { replace: true });
+  };
 
   const sendEmail = (e) => {
     console.log(e.target);
     e.preventDefault();
-    emailjs.sendForm('service_ha2zlgf', 'template_dca7vu4', e.target, 'I885yDqg2lLy-vOr_')
-    .then((result)=>{
-      console.log(result.text);
-    }, (error)=>{
-      console.log(error.text);
-    });
-  }
+    emailjs
+      .sendForm(
+        "service_ha2zlgf",
+        "template_dca7vu4",
+        e.target,
+        "I885yDqg2lLy-vOr_"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <>
@@ -36,7 +56,12 @@ export default function HelpAlerts() {
       </Helmet>
 
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
           <Typography variant="h3" gutterBottom>
             Help Alerts
           </Typography>
@@ -46,19 +71,58 @@ export default function HelpAlerts() {
           Send Help Alert
         </Typography>
 
-
-    <Stack alignItems="center" justifyContent="space-between" mb={5} ml={5} sx={{width:'90%'}} >
-      <form onSubmit={sendEmail} style={{marginTop: "5px", alignItems:'center', justifyContent:'center'}} >
-        <TextField id="outlined-basic" variant="outlined" sx={{width:'80%', marginBottom:1,}}  type="text" name="name" placeholder="Name"/>
-        <TextField id="outlined-basic" variant="outlined" sx={{width:'80%', marginBottom:1,}} type="text" name="reply_to" placeholder="Email"/>
-        <TextField id="outlined-basic" variant="outlined" multiline rows={6} sx={{width:'80%', marginBottom:1,}} placeholder={'Write message here...'} name="message" />
-        <br></br>
-        <Button variant="contained" sx={{width:'30%', marginBottom:1,}} type='submit' className="submit__btn" style={{marginTop: "5px", marginLeft:'25%'}}>
-          Send
-        </Button>
-      </form>
-      </Stack>
-
+        <Stack
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+          ml={5}
+          sx={{ width: "90%" }}
+        >
+          <form
+            onSubmit={sendEmail}
+            style={{
+              marginTop: "5px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              sx={{ width: "80%", marginBottom: 1 }}
+              type="text"
+              name="name"
+              placeholder="Name"
+            />
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              sx={{ width: "80%", marginBottom: 1 }}
+              type="text"
+              name="reply_to"
+              placeholder="Email"
+            />
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              multiline
+              rows={6}
+              sx={{ width: "80%", marginBottom: 1 }}
+              placeholder={"Write message here..."}
+              name="message"
+            />
+            <br></br>
+            <Button
+              variant="contained"
+              sx={{ width: "30%", marginBottom: 1 }}
+              type="submit"
+              className="submit__btn"
+              style={{ marginTop: "5px", marginLeft: "25%" }}
+            >
+              Send
+            </Button>
+          </form>
+        </Stack>
 
         {/* <Stack alignItems="center" justifyContent="space-between" mb={5} ml={5} sx={{width:'90%'}} >
 
@@ -73,42 +137,75 @@ export default function HelpAlerts() {
           Previous Help Alerts
         </Typography>
 
-          <Container sx={{marginTop:2}} >
-            <Divider>
-              <Typography variant='subtitle1' >Jan 10, 2023 10:30 AM</Typography>
-            </Divider>
-            <Typography variant='body2' sx={{textAlign:'justify'}} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam optio eius explicabo harum delectus, cupiditate, autem maiores magni repellat minus ea sapiente reprehenderit facilis iusto necessitatibus velit blanditiis accusamus facere!
-            Facere eveniet et natus voluptates itaque quod, optio, at labore esse molestiae vitae earum? Recusandae, dolor sit numquam alias nostrum quo id, asperiores necessitatibus dolorem corrupti quos ipsum consequatur unde?
-            Odio sunt in delectus, veniam qui debitis necessitatibus. Odit numquam sit praesentium repellendus voluptate? Culpa, magni beatae dolore tenetur, voluptas eligendi, earum ullam ad et facere maiores eum mollitia inventore?
-            Magnam perferendis vel sed ea nesciunt. Saepe repellendus at quisquam ullam reiciendis! Laudantium perferendis expedita quibusdam veritatis sequi quia, nihil, officia in blanditiis aperiam tempore animi assumenda, nemo ipsam ipsum?</Typography>
-          </Container>
+        <Container sx={{ marginTop: 2 }}>
+          <Divider>
+            <Typography variant="subtitle1">Jan 10, 2023 10:30 AM</Typography>
+          </Divider>
+          <Typography variant="body2" sx={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+            optio eius explicabo harum delectus, cupiditate, autem maiores magni
+            repellat minus ea sapiente reprehenderit facilis iusto
+            necessitatibus velit blanditiis accusamus facere! Facere eveniet et
+            natus voluptates itaque quod, optio, at labore esse molestiae vitae
+            earum? Recusandae, dolor sit numquam alias nostrum quo id,
+            asperiores necessitatibus dolorem corrupti quos ipsum consequatur
+            unde? Odio sunt in delectus, veniam qui debitis necessitatibus. Odit
+            numquam sit praesentium repellendus voluptate? Culpa, magni beatae
+            dolore tenetur, voluptas eligendi, earum ullam ad et facere maiores
+            eum mollitia inventore? Magnam perferendis vel sed ea nesciunt.
+            Saepe repellendus at quisquam ullam reiciendis! Laudantium
+            perferendis expedita quibusdam veritatis sequi quia, nihil, officia
+            in blanditiis aperiam tempore animi assumenda, nemo ipsam ipsum?
+          </Typography>
+        </Container>
 
-          <Container sx={{marginTop:2}} >
-            <Divider>
-              <Typography variant='subtitle1' >Jan 10, 2023 10:30 AM</Typography>
-            </Divider>
-            <Typography variant='body2' sx={{textAlign:'justify'}} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam optio eius explicabo harum delectus, cupiditate, autem maiores magni repellat minus ea sapiente reprehenderit facilis iusto necessitatibus velit blanditiis accusamus facere!
-            Facere eveniet et natus voluptates itaque quod, optio, at labore esse molestiae vitae earum? Recusandae, dolor sit numquam alias nostrum quo id, asperiores necessitatibus dolorem corrupti quos ipsum consequatur unde?
-            Odio sunt in delectus, veniam qui debitis necessitatibus. Odit numquam sit praesentium repellendus voluptate? Culpa, magni beatae dolore tenetur, voluptas eligendi, earum ullam ad et facere maiores eum mollitia inventore?
-            Magnam perferendis vel sed ea nesciunt. Saepe repellendus at quisquam ullam reiciendis! Laudantium perferendis expedita quibusdam veritatis sequi quia, nihil, officia in blanditiis aperiam tempore animi assumenda, nemo ipsam ipsum?</Typography>
-          </Container>
+        <Container sx={{ marginTop: 2 }}>
+          <Divider>
+            <Typography variant="subtitle1">Jan 10, 2023 10:30 AM</Typography>
+          </Divider>
+          <Typography variant="body2" sx={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+            optio eius explicabo harum delectus, cupiditate, autem maiores magni
+            repellat minus ea sapiente reprehenderit facilis iusto
+            necessitatibus velit blanditiis accusamus facere! Facere eveniet et
+            natus voluptates itaque quod, optio, at labore esse molestiae vitae
+            earum? Recusandae, dolor sit numquam alias nostrum quo id,
+            asperiores necessitatibus dolorem corrupti quos ipsum consequatur
+            unde? Odio sunt in delectus, veniam qui debitis necessitatibus. Odit
+            numquam sit praesentium repellendus voluptate? Culpa, magni beatae
+            dolore tenetur, voluptas eligendi, earum ullam ad et facere maiores
+            eum mollitia inventore? Magnam perferendis vel sed ea nesciunt.
+            Saepe repellendus at quisquam ullam reiciendis! Laudantium
+            perferendis expedita quibusdam veritatis sequi quia, nihil, officia
+            in blanditiis aperiam tempore animi assumenda, nemo ipsam ipsum?
+          </Typography>
+        </Container>
 
-          <Container sx={{marginTop:2}} >
-            <Divider>
-              <Typography variant='subtitle1' >Jan 10, 2023 10:30 AM</Typography>
-            </Divider>
-            <Typography variant='body2' sx={{textAlign:'justify'}} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam optio eius explicabo harum delectus, cupiditate, autem maiores magni repellat minus ea sapiente reprehenderit facilis iusto necessitatibus velit blanditiis accusamus facere!
-            Facere eveniet et natus voluptates itaque quod, optio, at labore esse molestiae vitae earum? Recusandae, dolor sit numquam alias nostrum quo id, asperiores necessitatibus dolorem corrupti quos ipsum consequatur unde?
-            Odio sunt in delectus, veniam qui debitis necessitatibus. Odit numquam sit praesentium repellendus voluptate? Culpa, magni beatae dolore tenetur, voluptas eligendi, earum ullam ad et facere maiores eum mollitia inventore?
-            Magnam perferendis vel sed ea nesciunt. Saepe repellendus at quisquam ullam reiciendis! Laudantium perferendis expedita quibusdam veritatis sequi quia, nihil, officia in blanditiis aperiam tempore animi assumenda, nemo ipsam ipsum?</Typography>
-          </Container>
+        <Container sx={{ marginTop: 2 }}>
+          <Divider>
+            <Typography variant="subtitle1">Jan 10, 2023 10:30 AM</Typography>
+          </Divider>
+          <Typography variant="body2" sx={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+            optio eius explicabo harum delectus, cupiditate, autem maiores magni
+            repellat minus ea sapiente reprehenderit facilis iusto
+            necessitatibus velit blanditiis accusamus facere! Facere eveniet et
+            natus voluptates itaque quod, optio, at labore esse molestiae vitae
+            earum? Recusandae, dolor sit numquam alias nostrum quo id,
+            asperiores necessitatibus dolorem corrupti quos ipsum consequatur
+            unde? Odio sunt in delectus, veniam qui debitis necessitatibus. Odit
+            numquam sit praesentium repellendus voluptate? Culpa, magni beatae
+            dolore tenetur, voluptas eligendi, earum ullam ad et facere maiores
+            eum mollitia inventore? Magnam perferendis vel sed ea nesciunt.
+            Saepe repellendus at quisquam ullam reiciendis! Laudantium
+            perferendis expedita quibusdam veritatis sequi quia, nihil, officia
+            in blanditiis aperiam tempore animi assumenda, nemo ipsam ipsum?
+          </Typography>
+        </Container>
 
-          {/* <Grid container >
+        {/* <Grid container >
             <Button>Check</Button>
           </Grid> */}
-
-
-
 
         {/* <Grid container spacing={3}>
           {POSTS.map((post, index) => (
